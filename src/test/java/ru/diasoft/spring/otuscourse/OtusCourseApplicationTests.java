1fdae32ff5a661ca9a9840628c45a6d08ca13cb1
avergun <avergun@diasoft.ru>
2022-06-23 23:17:23 +0300

    init

---
+++ src/test/java/ru/diasoft/spring/otuscourse/OtusCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.spring.otuscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
