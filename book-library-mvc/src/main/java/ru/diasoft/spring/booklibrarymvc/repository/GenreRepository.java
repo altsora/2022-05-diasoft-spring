ea1f427f1c58768bdd4dfa93fc9d5dd24cc77f74
avergun <avergun@diasoft.ru>
2022-08-25 23:51:50 +0300

    4.1 Монолиты vs. Microservices (Round 2), Spring Boot Actuator - must have в микросервисах

--- book-library-mvc/src/main/java/ru/diasoft/spring/booklibrarymvc/repository/GenreRepository.java
+++ book-library-mvc/src/main/java/ru/diasoft/spring/booklibrarymvc/repository/GenreRepository.java
@@ -1,10 +1,12 @@
 package ru.diasoft.spring.booklibrarymvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ru.diasoft.spring.booklibrarymvc.domain.Genre;
 
 @Repository
+@RepositoryRestResource(path = "genre")
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
